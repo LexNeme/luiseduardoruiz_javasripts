dae7206979a133d9a002a76ba4d3654dfc626e5a
Luis Eduardo <luiggis@hotmail.com>
2023-05-19 16:22:42 -0500

    review 1.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 // Declarar variables
-let productoElegido;
-let cantidad;
-let precioUnitario;
+let nombreUsuario;
+let productosComprados = [];
+let continuarComprando = true;
+
+// Objeto de productos disponibles
+const productosDisponibles = [
+  { nombre: "Chaqueta de cuero", precio: 250 },
+  { nombre: "Bolso de cuero", precio: 150 },
+  { nombre: "Zapatos de cuero", precio: 135 },
+  { nombre: "Cinturón de cuero", precio: 25 }
+];
 
 // Función para mostrar productos disponibles y solicitar elección
 function mostrarProductos() {
-  return parseInt(prompt("Ingrese el número del producto que desea comprar:\n1. Chaqueta de cuero\n2. Bolso de cuero\n3. Zapatos de cuero\n4. Cinturón de cuero"));
+  let opciones = "";
+  for (let i = 0; i < productosDisponibles.length; i++) {
+    opciones += `${i + 1}. ${productosDisponibles[i].nombre}\n`;
+  }
+  return parseInt(prompt(`Ingrese el número del producto que desea comprar:\n${opciones}`));
 }
 
 // Función para solicitar cantidad de productos
@@ -14,54 +26,67 @@ function solicitarCantidad() {
 }
 
 // Mostrar mensaje de bienvenida
-prompt("¡Bienvenido a la tienda en línea de cuero! \n precionar aceptar para continuar");
+prompt("¡Bienvenido a la tienda en línea de cuero!, presione aceptar");
 
-// Mostrar productos disponibles y solicitar elección
-productoElegido = mostrarProductos();
-
-while (productoElegido < 1 || productoElegido > 4) {
-  prompt("El número de producto ingresado no es válido.");
-  productoElegido = mostrarProductos();
+// Solicitar nombre del usuario
+while (!nombreUsuario) {
+  nombreUsuario = prompt("Por favor, ingrese su nombre:");
 }
 
-// Asignar precio unitario según el producto elegido
-if (productoElegido === 1) {
-  precioUnitario = 250;
-  prompt("Ha elegido una chaqueta de cuero.");
-} else if (productoElegido === 2) {
-  precioUnitario = 150;
-  prompt("Ha elegido un bolso de cuero.");
-} else if (productoElegido === 3) {
-  precioUnitario = 135;
-  prompt("Ha elegido unos zapatos de cuero.");
-} else if (productoElegido === 4) {
-  precioUnitario = 25;
-  prompt("Ha elegido un cinturón de cuero.");
+// Bucle para comprar productos
+while (continuarComprando) {
+  // Mostrar productos disponibles y solicitar elección
+  let productoElegido = mostrarProductos();
+
+  while (productoElegido < 1 || productoElegido > productosDisponibles.length) {
+    prompt("El número de producto ingresado no es válido.");
+    productoElegido = mostrarProductos();
+  }
+
+  // Obtener producto seleccionado
+  let producto = productosDisponibles[productoElegido - 1];
+
+  // Solicitar cantidad al usuario
+  let cantidad = solicitarCantidad();
+
+  // Calcular el total de la compra
+  let total = producto.precio * cantidad;
+
+  // Agregar producto y cantidad a la lista de compras
+  productosComprados.push({
+    nombre: producto.nombre,
+    cantidad: cantidad,
+    precioUnitario: producto.precio,
+    total: total
+  });
+
+  // Solicitar confirmación de compra
+  let confirmacion = prompt("¿Desea continuar comprando? (Ingrese 'si' o 'no')");
+
+  while (confirmacion !== "si" && confirmacion !== "no") {
+    confirmacion = prompt("Respuesta inválida. ¿Desea continuar comprando? (Ingrese 'si' o 'no')");
+  }
+
+  if (confirmacion === "no") {
+    continuarComprando = false;
+  }
 }
 
-// Solicitar confirmación de compra
-let confirmacion = prompt("¿Desea continuar con la compra? (Ingrese 'si' o 'no')");
+// Mostrar resumen de la compra
+let resumenCompra = `Resumen de la compra de ${nombreUsuario}:\n\n`;
+let totalCompra = 0;
 
-while (confirmacion !== "si" && confirmacion !== "no") {
-  confirmacion = prompt("Respuesta inválida. ¿Desea continuar con la compra? (Ingrese 'si' o 'no')");
+for (let i = 0; i < productosComprados.length; i++) {
+  let producto = productosComprados[i];
+  resumenCompra += `Producto: ${producto.nombre}\n`;
+  resumenCompra += `Cantidad: ${producto.cantidad}\n`;
+  resumenCompra += `Precio unitario: $${producto.precioUnitario}\n`;
+  resumenCompra += `Total: $${producto.total}\n\n`;
+  totalCompra += producto.total;
 }
 
-if (confirmacion === "no") {
-  productoElegido = mostrarProductos();
-}
+resumenCompra += `Total de la compra: $${totalCompra}`;
 
-// Solicitar cantidad al usuario
-cantidad = solicitarCantidad();
+prompt(resumenCompra);
 
-// Calcular el total de la compra
-let total = precioUnitario * cantidad;
-
-// Mostrar el total de la compra
-prompt("El total de la compra es: $" + total);
-
-// Bucle for para imprimir un mensaje de agradecimiento por cada producto comprado
-for (let i = 0; i < cantidad; i++) {
-  prompt("¡Gracias por comprar nuestro producto de cuero!");
-}
-
-prompt("¡Esperamos que disfrute su compra!");
+prompt("¡Gracias por su compra, " + nombreUsuario + "! Esperamos que disfrute de sus productos de cuero.");
